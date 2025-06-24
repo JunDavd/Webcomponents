f765f5c486dd0423e5ec5807642e408bb469235a
Juan Ruiz <jdruiz008@gmail.com>
2025-06-24 17:48:27 -0500

    todo app, componentes input-action.js todo-item.js

---
+++ 1.5_todo-list-app/input-action.js
@@ -0,0 +1,49 @@
+const template = document.createElement('template')
+template.innerHTML = `
+<div class="input-action">
+    <input type="text">
+    <button disabled></button>
+</div>
+`
+
+
+class InputAction extends HTMLElement{
+    constructor(){
+        super()
+        this.buttonLabel = this.getAttribute('button-label') ?? 'Add'
+        this.placeholder = this.getAttribute('placeholder') ?? 'Escribe algo que tengas pendiente de hacer'
+        this.attachShadow({mode:"open"})
+    }
+
+    connectedCallback(){
+        const templateCopy = template.content.cloneNode(true)
+        const input = templateCopy.querySelector('input')
+        const button = templateCopy.querySelector('button')
+
+        input.setAttribute('placeholder',this.placeholder)
+        button.textContent = this.buttonLabel
+
+        button.addEventListener('click', () =>{
+            const inputValue = input.value
+            const event = new CustomEvent('input-action-submit', {
+                detail: inputValue
+            })
+            this.dispatchEvent(event)
+            input.value = ''
+            button.setAttribute('disabled', '')
+        })
+
+        input.addEventListener('input', () => {
+            const currentValue = input.value
+            if(currentValue){
+                button.removeAttribute('disabled')
+            } else {
+                button.setAttribute('disabled', '')
+            }
+        })
+        this.shadowRoot.appendChild(templateCopy)
+    }
+
+}
+
+window.customElements.define('input-action',InputAction)
---
+++ 1.5_todo-list-app/todo-item.js
@@ -0,0 +1,53 @@
+const template = document.createElement('template')
+template.innerHTML = `
+<style>
+        input:checked + span{
+            text-decoration: line-through;
+        }
+
+    </style>
+<div class="todo-item">
+    <input type="checkbox">
+      <span></span>
+      <button></button>
+</div>
+`
+
+
+class TodoItem extends HTMLElement{
+    constructor(){
+        super()
+        this.text = this.getAttribute("text") ?? "Todo item"
+        this.isChecked = this.hasAttribute("isChecked")
+        this.buttonLabel = this.getAttribute('button-label') ?? 'Eliminar'
+
+        this.attachShadow({mode:"open"})
+    }
+
+    connectedCallback(){
+        const templateCopy = template.content.cloneNode(true)
+        const checkbox = templateCopy.querySelector('input')
+        const removeButtonon = templateCopy.querySelector('button')
+
+        templateCopy.querySelector('span').textContent = this.text
+        checkbox.checked = this.isChecked
+        removeButtonon.textContent = this.buttonLabel
+        removeButtonon.addEventListener('click', () => {
+            const event = new CustomEvent('todo-item-remove')
+            this.dispatchEvent(event)
+            this.remove()
+        })
+        checkbox.addEventListener('change', () =>{
+            const event = new CustomEvent('todo-item-status-update', {
+                detail: {
+                    isChecked: checkbox.checked
+                }
+            })
+            this.dispatchEvent(event)
+        })
+
+        this.shadowRoot.appendChild(templateCopy)
+    }
+}
+
+window.customElements.define('todo-item',TodoItem)
